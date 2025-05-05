bc4b46ad722365bb3ed72e9dbf4c5446d0dcc75b
Ali Yağız Uz <aliyagizuz@gmail.com>
2025-05-05 19:02:19 +0300

    eklenti

--- ders1/src/HelloWorld.java
+++ ders1/src/HelloWorld.java
@@ -2,8 +2,12 @@ public class HelloWorld {
 
     public static void main(String[] args) {
 
-        System.out.println("Merhaba Dunya!!");
+        for (int a = 0 ; a < 10 ; a++){
 
+            System.out.println("Merhaba Dunya!!");
+
+
+        }
 
     }
 
