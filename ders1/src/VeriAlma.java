facdc6ed8280a2cabb7089f267bc792449cf6c97
Ali Yağız Uz <aliyagizuz@gmail.com>
2025-05-05 21:17:39 +0300

    veri alma eklendi

---
+++ ders1/src/VeriAlma.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+//import java.util.*; // Tüm sınıfları ekler.
+
+public class VeriAlma {
+
+    public static void main(String[] args) {
+
+    Scanner girdi = new Scanner(System.in);
+
+        System.out.print("A sayısını giriniz   : ");
+        int a = girdi.nextInt();
+        System.out.print("\nb sayıını giriniz : ");
+        int b = girdi.nextInt();
+
+        int toplam = a + b;
+        System.out.println("\n"+toplam);
+
+
+
+    }
+
+
+}
