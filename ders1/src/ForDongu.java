a095314a3344a77addca1241e063c1c0524e47d0
Ali Yağız Uz <aliyagizuz@gmail.com>
2025-05-05 19:13:31 +0300

    Last branch added

---
+++ ders1/src/ForDongu.java
@@ -0,0 +1,15 @@
+public class ForDongu {
+    public static void main(String[] args){
+        int i;
+        for (i=0; i<=10; i++){
+
+            System.out.println(i);
+
+        }
+
+
+    }
+
+
+
+}
