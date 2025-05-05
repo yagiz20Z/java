9f1d751e7480ade7322e8af76fa8dcaa54ccfd6d
Ali Yağız Uz <aliyagizuz@gmail.com>
2025-05-05 20:02:18 +0300

    eklemeler

--- ders1/src/HelloWorld.java
+++ ders1/src/HelloWorld.java
@@ -4,6 +4,16 @@ public class HelloWorld {
 
         System.out.println("Merhaba Dunya!!");
 
+        System.out.println("printfln bu ifade yazdırır ve alt satıra geçer"
+                + " ayrıyeten bir ozellik olarak da daha rahat yazmayı"
+                + "bu sekilde saglayabiliriz");
+        // yazarken entere basılınca otomatik olarak '+' ekleyip alt satıra geçer
+
+
+        System.out.println(10+25); // direkt sayıların 'int' olduğunu algılar ve toplamana bile izin verir
+
+        System.out.print("bak\n\tböyle de kullanabilirsin");
+
 
     }
 
